8b79cce4fc690e6b30380aa7a91d9523f4963d75
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2025-02-05 08:36:30 +0100

    created a simple navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <div className="navbar">
+        <div className="title">
+            <h1>123Movies</h1>
+        </div>
+        <div className="navlist">
+            <ul>
+                <li>Home</li>
+            </ul>
+        </div>
+    </div>
+  );
+}
+
+export default Navbar;
